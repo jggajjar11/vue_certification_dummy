e66af70ba30b33a88fc50252fc7b59438d98fc92
jggajjar11 <jainish9gajjar@gmail.com>
2024-07-07 20:29:10 +0530

    Changes:(edited the countdowndate)

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 31,2024 5:15:56").getTime();
+var countDownDate = new Date("Jan 01,2025 8:15:56").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
